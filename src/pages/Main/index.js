e5124ef923a679ce9a807e3bf4bbede423fb42ef
MoisesMorandini <moisesmorandini@moisesmorandini.local>
2019-09-08 22:46:43 -0300

    desafio 055

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
 import api from '../../services/api';
-import { Container, Form, SubmitButton } from './styles';
+import Container from '../../components/Container';
+import { Form, SubmitButton, List } from './styles';
 
 export default class Main extends Component {
   state = {
     newRepo: '',
     repositories: [],
-    loading: false,
+    loading: 0,
+    notFound: false,
   };
+
+  // carregar os dados do localstorage
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  // salvar os dados do localstore
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
 
   handleInputChange = e => {
     this.setState({ newRepo: e.target.value });
@@ -17,31 +37,42 @@ export default class Main extends Component {
   handleSubmit = async e => {
     e.preventDefault();
 
-    this.setState({ loading: true });
+    this.setState({ loading: 1, notFound: false });
 
     const { newRepo, repositories } = this.state;
 
-    const response = await api.get(`/repos/${newRepo}`);
-    const data = {
-      name: response.data.full_name,
-    };
+    try {
+      const hasRepo = repositories.find(repo => repo.name === newRepo);
 
-    this.setState({
-      repositories: [...repositories, data],
-      newRepo: '',
-      loading: false,
-    });
+      if (hasRepo) throw 'Repositório duplicado';
+
+      const response = await api.get(`/repos/${newRepo}`);
+      const data = {
+        name: response.data.full_name,
+      };
+
+      this.setState({
+        repositories: [...repositories, data],
+        newRepo: '',
+        loading: 0,
+      });
+    } catch (error) {
+      this.setState({
+        loading: 0,
+        notFound: true,
+      });
+    }
   };
 
   render() {
-    const { newRepo, loading } = this.state;
+    const { newRepo, repositories, loading, notFound } = this.state;
     return (
       <Container>
         <h1>
           <FaGithubAlt />
           Repositórios
         </h1>
-        <Form onSubmit={this.handleSubmit}>
+        <Form onSubmit={this.handleSubmit} notFound={notFound}>
           <input
             value={newRepo}
             onChange={this.handleInputChange}
@@ -56,6 +87,16 @@ export default class Main extends Component {
             )}
           </SubmitButton>
         </Form>
+        <List>
+          {repositories.map(repositorie => (
+            <li key={repositorie.name}>
+              <span>{repositorie.name}</span>
+              <Link to={`/repository/${encodeURIComponent(repositorie.name)}`}>
+                Detalhes
+              </Link>
+            </li>
+          ))}
+        </List>
       </Container>
     );
   }
--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,5 +1,125 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
+import { Loading, Owner, IssueList, Button, ButtonPrevios } from './styles';
 
-export default function Repository() {
-  return <h1>Repository</h1>;
+import Container from '../../components/Container';
+
+export default class Repository extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired,
+  };
+
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+    page: 1,
+    isFirst: true,
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          page: this.state.page,
+        },
+      }),
+
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  async componentDidUpdate(_, prevState) {
+    const { page } = this.state;
+    if(page !== prevState.page){
+
+        const { match } = this.props;
+        const repoName = decodeURIComponent(match.params.repository);
+        const issues = await api.get(`/repos/${repoName}/issues`, {
+          params: {
+            page: this.state.page,
+          },
+        })
+
+        this.setState({
+          issues: issues.data,
+          loading: false,
+          isFirst: false,
+        });
+
+        if(page===1){
+        console.log('caiu')
+        this.setState({
+          isFirst: true,
+        });
+      }
+
+    }
+  }
+
+  async changeResult(num) {
+    const { page } = this.state;
+    this.setState({
+      page: page + num,
+    });
+
+  }
+
+  render() {
+    const { repository, issues, loading, isFirst } = this.state;
+
+    if (loading) {
+      return <Loading>Carregando</Loading>;
+    }
+
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Voltar aos repositórios</Link>
+          <img
+            src={repository.owner.avatar_url}
+            alt={repository.owner.loading}
+          />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+        <Button >
+          <button onClick={() => this.changeResult(1)}>Próxima</button>
+          <ButtonPrevios isFirst={isFirst} onClick={() => this.changeResult(-1)}>Anterior</ButtonPrevios>
+        </Button>
+        <IssueList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map(label => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
+      </Container>
+    );
+  }
 }
